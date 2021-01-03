943213548964ff5581147f6dd549ae431a1a3cee
Jestfer <jestfer6@gmail.com>
2021-01-03 10:16:50 +0000

    installing node and pm2 on reload script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # cd ~/aws-codedeploy
 cd ../../var/www/html/
-# npm install pm2 -g
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-sudo pm2 startOrReload ecosystem.config.json
-# pm2 startOrReload ecosystem.config.json
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install pm2 -g
+
+pm2 startOrReload ecosystem.config.json
 # ~/.nvm/versions/node/v15.4.0/bin/pm2 startOrReload ecosystem.config.json
