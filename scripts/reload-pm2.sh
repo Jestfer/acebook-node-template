820d8d1a88d9ca694a3e92c8646612b2e5fbbc3b
Jestfer <jestfer6@gmail.com>
2021-01-03 00:48:26 +0000

    using full path to pm2

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # cd ~/aws-codedeploy
 cd ../../var/www/html/
-
-# node ./bin/www
-pm2 startOrReload ecosystem.config.json
+/usr/local/bin/pm2 startOrReload ecosystem.config.json
+# pm2 startOrReload ecosystem.config.json
