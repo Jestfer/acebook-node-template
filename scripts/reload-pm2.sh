dca7d519dcee32366a9c73274fd88b96a797c495
Jestfer <jestfer6@gmail.com>
2021-01-03 01:09:58 +0000

    last now

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -2,7 +2,8 @@
 # cd ~/aws-codedeploy
 cd ../../var/www/html/
 # npm install pm2 -g
-# ~/.nvm/versions/node/v15.4.0/bin/pm2 startOrReload ecosystem.config.json
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-pm2 startOrReload ecosystem.config.json
+# pm2 startOrReload ecosystem.config.json
+~/.nvm/versions/node/v15.4.0/bin/pm2 startOrReload ecosystem.config.json
