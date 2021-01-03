d72a4613deb2e28673c3d3a793380207be149c3a
Jestfer <jestfer6@gmail.com>
2021-01-03 12:03:09 +0000

    cleanup files

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# cd ~/aws-codedeploy
 cd ../../var/www/html/
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
@@ -8,4 +7,3 @@ nvm install node
 npm install pm2 -g
 
 pm2 startOrReload ecosystem.config.json
-# ~/.nvm/versions/node/v15.4.0/bin/pm2 startOrReload ecosystem.config.json
