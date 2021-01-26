b541c2981db544e83fa5528c99846b68da444e70
Jestfer <jestfer6@gmail.com>
2021-01-26 14:33:44 +0000

    updating script

--- scripts/load-docker-image.sh
+++ scripts/load-docker-image.sh
@@ -1,8 +1,15 @@
 cd ../../var/www/html/
 
-# yum install -y docker
-
+yum install -y docker
 systemctl start docker
+
+echo "Is there a tmp folder inside /var/lib/docker/?"
+ls /var/lib/docker/tmp/
+# mkdir /var/lib/docker/tmp/
+
+echo "I get to Docker Load"
+echo "Let's check what's in here"
+ls
 
 # docker load -i /path/to/package/acebook-node-docker.tar.gz
 docker load -i acebook-node-docker.tar.gz
