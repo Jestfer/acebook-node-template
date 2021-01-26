55537b968af6d774d7f159287e3df4204b187eec
Jestfer <jestfer6@gmail.com>
2021-01-26 20:11:15 +0000

    running image on container

--- scripts/load-docker-image.sh
+++ scripts/load-docker-image.sh
@@ -3,21 +3,21 @@ cd ../../var/www/html/
 yum install -y docker
 systemctl start docker
 
-# https://github.com/moby/moby/issues/19566#issuecomment-307678645
-echo "There is a tmp folder inside /var/lib/docker/"
+# # https://github.com/moby/moby/issues/19566#issuecomment-307678645
+# echo "There is a tmp folder inside /var/lib/docker/"
 
-# https://stackoverflow.com/a/63378252/8243590
-echo "Could the issue be the file format? Trying with .tar now"
+# # https://stackoverflow.com/a/63378252/8243590
+# echo "Could the issue be the file format? Trying with .tar now"
 
-echo "I get to Docker Load"
-echo "Let's check what's in here"
-ls
+# echo "I get to Docker Load"
+# echo "Let's check what's in here"
+# ls
 
-echo "And let's check what's in lib/docker"
-ls -la /var/lib/docker/
+# echo "And let's check what's in lib/docker"
+# ls -la /var/lib/docker/
 
-echo "And let's check what's in tmp folder"
-ls -la /var/lib/docker/tmp/
+# echo "And let's check what's in tmp folder"
+# ls -la /var/lib/docker/tmp/
 
 echo "Any Docker images?"
 docker images
@@ -30,3 +30,8 @@ docker images
 
 # https://gist.github.com/monkeym4ster/492af14ad078d005c8349bf1c97fdac5
 docker load -i acebook-node-docker.tgz
+
+echo "Any Docker images now?"
+docker images
+
+docker run -d josuemakers/acebook-node
