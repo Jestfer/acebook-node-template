693e02aa80cbd261800ef0b9a093e92f917750a3
Jestfer <jestfer6@gmail.com>
2021-01-26 14:42:04 +0000

    added missing tmp folder in var-lib-docker

--- scripts/load-docker-image.sh
+++ scripts/load-docker-image.sh
@@ -5,7 +5,8 @@ systemctl start docker
 
 echo "Is there a tmp folder inside /var/lib/docker/?"
 ls /var/lib/docker/tmp/
-# mkdir /var/lib/docker/tmp/
+# Creating dir as specified in https://github.com/moby/moby/issues/19566#issuecomment-307678645
+mkdir /var/lib/docker/tmp/
 
 echo "I get to Docker Load"
 echo "Let's check what's in here"
